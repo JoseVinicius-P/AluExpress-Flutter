e024e5e35b9be308d51ab8b2f70cce0e5dda8ddd
JoseVinicius-P <josevpf123@gmail.com>
2023-10-10 22:03:41 -0300

    style: Criando tela de apresentação

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -6,9 +6,17 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Flutter Slidy',
-      theme: ThemeData(primarySwatch: Colors.blue),
       routerDelegate: Modular.routerDelegate,
       routeInformationParser: Modular.routeInformationParser,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        textTheme: const TextTheme(
+          titleMedium: TextStyle(fontSize: 40, color: Colors.white),
+          titleLarge: TextStyle(fontSize: 70, color: Colors.green, fontWeight: FontWeight.bold),
+          labelSmall: TextStyle(fontSize: 15, color: Colors.white),
+        ),
+      ),
     );
   }
 }
--- lib/app/modules/presentation/presentation_page.dart
+++ lib/app/modules/presentation/presentation_page.dart
@@ -13,12 +13,65 @@ class PresentationPageState extends State<PresentationPage> {
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height,
+              color: Colors.white,
+              child: const Image(
+                image: AssetImage('assets/images/presentation_background.jpg'),
+                fit: BoxFit.cover,),
+            ),
+            Column(
+              children: [
+                const Spacer(),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.transparent, Colors.black.withOpacity(0.5), Colors.black.withOpacity(0.7)],
+                      ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 25,),
+                        Text(
+                          "Bem Vindo ao 👋",
+                          style: theme.textTheme.titleMedium!.copyWith(),
+                          textAlign: TextAlign.left,
+                        ),
+                        const SizedBox(height: 10,),
+                        Text(
+                          "Luguel",
+                          style: theme.textTheme.titleLarge!.copyWith(),
+                          textAlign: TextAlign.left,
+                        ),
+                        const SizedBox(height: 25,),
+                        Text(
+                          "Encontre seu lar perfeito! Onde a Casa dos seus sonhos se torna um aluguel da realidade!",
+                          style: theme.textTheme.labelSmall!.copyWith(),
+                          textAlign: TextAlign.left,
+                        ),
+                        const SizedBox(height: 30,),
+                      ],
+                    ),
+                  ),
+                ),
+
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
