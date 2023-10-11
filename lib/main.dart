b2f4de4d7c23aefec3e2658bf150b7e097c15a30
JoseVinicius-P <josevpf123@gmail.com>
2023-10-11 20:05:23 -0300

    feat: Possibitando a ultilização de tamanho relativos ao tamanho da tela do dispositivo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() => runApp(ModularApp(module: AppModule(), child: AppWidget()));
+void main(){
+  runApp(
+    //Para facilitar implementação de responsividade
+    ResponsiveApp(
+      builder: (context) =>
+        ModularApp(
+          module: AppModule(),
+          child: AppWidget()
+        )
+    )
+  );
+}
