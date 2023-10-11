42cd3908f95c1050178af06bb090d276448739cb
JoseVinicius-P <josevpf123@gmail.com>
2023-10-11 20:12:22 -0300

    style: Definindo cor padrão

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:luguel/app/shared/my_colors.dart';
 
 class AppWidget extends StatelessWidget {
   @override
@@ -13,7 +14,7 @@ class AppWidget extends StatelessWidget {
         useMaterial3: true,
         textTheme: const TextTheme(
           titleMedium: TextStyle(fontSize: 40, color: Colors.white),
-          titleLarge: TextStyle(fontSize: 70, color: Colors.green, fontWeight: FontWeight.bold),
+          titleLarge: TextStyle(fontSize: 70, color: MyColors.primaryColor, fontWeight: FontWeight.bold),
           labelSmall: TextStyle(fontSize: 15, color: Colors.white),
         ),
       ),
---
+++ lib/app/shared/my_colors.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+class MyColors {
+  static const primaryColor = Color(0xFF1AB65C);
+}
