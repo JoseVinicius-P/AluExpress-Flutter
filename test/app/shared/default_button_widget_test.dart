ce31bb2c1beea9ab007971666858496cde46dc58
JoseVinicius-P <josevpf123@gmail.com>
2023-10-12 15:25:58 -0300

    test: Adicionando parametro ao default button

--- test/app/shared/default_button_widget_test.dart
+++ test/app/shared/default_button_widget_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:luguel/app/shared/default_button_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:luguel/app/shared/my_colors.dart';
 import 'package:modular_test/modular_test.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
@@ -12,7 +13,17 @@ Widget buildTestableWidget(Widget widget) =>
 main() {
   group('WidgetsWidget', () {
     testWidgets('has a title and message', (WidgetTester tester) async {
-      await tester.pumpWidget(buildTestableWidget(DefaultButtonWidget(onTap: (){}, text: "teste",)));
+      await tester.pumpWidget(
+          buildTestableWidget(
+              DefaultButtonWidget(
+                onTap: (){},
+                text: "teste",
+                icon: const Icon(Icons.keyboard_arrow_right_rounded, color: Colors.white,),
+                background: MyColors.primaryColor,
+                textColor: Colors.white,
+              )
+          )
+      );
       final titleFinder = find.text('T');
       expect(titleFinder, findsOneWidget);
     });
