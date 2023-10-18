b5cf7e4e6722b98d8ab342d2e1a27c3e82a84846
JoseVinicius-P <josevpf123@gmail.com>
2023-10-18 20:15:06 -0300

    test: Criando teste para página que mostra opções de entrada

---
+++ test/app/modules/authentication/access_options_page_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:luguel/app/modules/authentication/access_options_page.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:modular_test/modular_test.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+// Função auxiliar para envolver os widgets a serem testados.
+Widget buildTestableWidget(Widget widget) =>
+    ResponsiveApp(builder: (context) => MaterialApp(home: widget));
+
+main() {
+  group('AccessOptionsPage', () {
+    testWidgets('has a title and message', (WidgetTester tester) async {
+      await tester.pumpWidget(buildTestableWidget(AccessOptionsPage(title: 'T')));
+      final titleFinder = find.text('T');
+      expect(titleFinder, findsOneWidget);
+    });
+  });
+}
