b3de598c4bae20b06b2ec185f7dd76756ce9cc3a
JoseVinicius-P <josevpf123@gmail.com>
2023-10-18 20:11:14 -0300

    feat: Criando modulo de autenticação

---
+++ lib/app/modules/authentication/authentication_module.dart
@@ -0,0 +1,12 @@
+import 'package:luguel/app/modules/authentication/authentication_page.dart';
+import 'package:luguel/app/modules/authentication/authentication_store.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AuthenticationModule extends Module {
+  @override
+  final List<Bind> binds = [Bind.lazySingleton((i) => AuthenticationStore()),];
+
+  @override
+  final List<ModularRoute> routes = [ChildRoute('/', child: (_, args) => AuthenticationPage()),];
+
+}
---
+++ lib/app/modules/authentication/authentication_store.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_triple/flutter_triple.dart';
+
+class AuthenticationStore extends Store<int> {
+
+  AuthenticationStore() : super(0);
+
+}
