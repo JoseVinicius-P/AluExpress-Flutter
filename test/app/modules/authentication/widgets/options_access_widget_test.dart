f55a6d1edd4972aa0143e165a7cf2b88bf6a6672
JoseVinicius-P <josevpf123@gmail.com>
2023-10-22 17:01:25 -0300

    test: Arquivos de testes gerados

---
+++ test/app/modules/authentication/widgets/options_access_widget_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:luguel/app/modules/authentication/widgets/options_access_widget.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:modular_test/modular_test.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+// Função auxiliar para envolver os widgets a serem testados.
+Widget buildTestableWidget(Widget widget) =>
+    ResponsiveApp(builder: (context) => MaterialApp(home: widget));
+
+main() {
+  group('OptionsAccessWidget', () {
+    testWidgets('has a title and message', (WidgetTester tester) async {
+      await tester.pumpWidget(buildTestableWidget(OptionsAccessWidget(onTapCreateAccount: (){}, onTapGoogleButton: (){}, onTapSignEmailWithPassword: (){},)));
+      final titleFinder = find.text('T');
+      expect(titleFinder, findsOneWidget);
+    });
+  });
+}
