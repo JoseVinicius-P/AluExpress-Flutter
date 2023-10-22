8a2ec355f8183d8b9322178b44122b599d929c38
JoseVinicius-P <josevpf123@gmail.com>
2023-10-22 19:53:35 -0300

    feat: Criando página e teste Create Account

---
+++ lib/app/modules/authentication/pages/create_account_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CreateAccountPage extends StatefulWidget {
+  final String title;
+  const CreateAccountPage({Key? key, this.title = 'CreateAccountPage'}) : super(key: key);
+  @override
+  CreateAccountPageState createState() => CreateAccountPageState();
+}
+class CreateAccountPageState extends State<CreateAccountPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
