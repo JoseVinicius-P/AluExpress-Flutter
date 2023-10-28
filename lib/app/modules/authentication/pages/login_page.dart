7f5f045f41f24544b4615c0cdf62f80944818448
JoseVinicius-P <josevpf123@gmail.com>
2023-10-28 19:13:50 -0300

    feat: Create Login Page

---
+++ lib/app/modules/authentication/pages/login_page.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:luguel/app/modules/authentication/widgets/divider_widget.dart';
+import 'package:luguel/app/modules/authentication/widgets/form_widget.dart';
+import 'package:luguel/app/modules/authentication/widgets/google_button_widget.dart';
+import 'package:luguel/app/modules/authentication/widgets/question_and_button_widget.dart';
+import 'package:luguel/app/modules/authentication/widgets/title_widget.dart';
+import 'package:luguel/app/shared/default_button_widget.dart';
+import 'package:luguel/app/shared/my_colors.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+  @override
+  LoginPageState createState() => LoginPageState();
+}
+class LoginPageState extends State<LoginPage> {
+
+  var questionAndButton = QuestionAndButtonWidget(
+    question: "Já tem uma conta?",
+    buttonText: "Fazer Login",
+    onPressed: (){},
+  );
+  var form = FormWidget(
+    onChangedEmail: (text){},
+    onChangedPassword: (text){},
+    onChangedRememberMe: (isChecked){},
+  );
+  var createAccountButton = DefaultButtonWidget(
+    onTap: (){},
+    text: 'Criar conta',
+    backgroundColor: MyColors.primaryColor,
+    textColor: Colors.white,
+    shadow: true,
+  );
+  var googleButton = GoogleButtonWidget(onTap: (){}, withText: false,);
+  final String title = 'Entrar na sua \nconta';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        forceMaterialTransparency: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(5.sw),
+        child: OrientationLayoutBuilder(
+          portrait: (context) {
+            return Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                    flex: 3,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Spacer(),
+                        const Flexible(child: SizedBox(height: 50,)),
+                        // ignore: prefer_const_constructors
+                        TitleWidget(
+                          text: title,
+                          textAlign: TextAlign.left,
+                        ),
+                        const Spacer(),
+                        form,
+                        createAccountButton,
+                        const Spacer(),
+                      ],
+                    )
+                ),
+                Expanded(
+                    flex: 1,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const DividerWidget(text: "ou continue com"),
+                        const Flexible(child: SizedBox(height: 15,)),
+                        Row(mainAxisSize: MainAxisSize.min, children: [googleButton],
+                        ),
+                        const Flexible(child: SizedBox(height: 25,)),
+                        questionAndButton,
+                      ],
+                    )
+                ),
+              ],
+            );
+          },
+          landscape: (context) {
+            return Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: TitleWidget(
+                    text: title,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Expanded(
+                    flex: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Spacer(),
+                        form,
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Flexible(child: googleButton),
+                            const SizedBox(width: 10,),
+                            Flexible(
+                                child: createAccountButton
+                            ),
+
+                          ],
+                        ),
+                        const Spacer(),
+                        questionAndButton,
+                      ],
+                    )
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ test/app/modules/authentication/pages/login_page_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:luguel/app/modules/authentication/pages/login_page.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:modular_test/modular_test.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+// Função auxiliar para envolver os widgets a serem testados.
+Widget buildTestableWidget(Widget widget) =>
+    ResponsiveApp(builder: (context) => MaterialApp(home: widget));
+
+
+main() {
+  group('LoginPage', () {
+    testWidgets('has a title and message', (WidgetTester tester) async {
+      await tester.pumpWidget(buildTestableWidget(const LoginPage()));
+      final titleFinder = find.text('T');
+      expect(titleFinder, findsOneWidget);
+    });
+  });
+}
