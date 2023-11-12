34938eb889344b60c6af30bfeaf1f7b40896cb02
JoseVinicius-P <josevpf123@gmail.com>
2023-11-11 22:49:40 -0300

    refactor: Corrigindo campos estaticos

--- lib/app/modules/authentication/interfaces/i_forgot_password_store.dart
+++ lib/app/modules/authentication/interfaces/i_forgot_password_store.dart
@@ -1,9 +1,6 @@
 import 'package:flutter_triple/flutter_triple.dart';
 
 abstract class IForgotPasswordStore extends Store<int>{
-  final int emailMethod = 0;
-  final int smsMethod = 1;
-
   IForgotPasswordStore(super.initialState);
 
   void selectContactMethod(int contactMethod);
--- lib/app/modules/authentication/stores/forgot_password_store.dart
+++ lib/app/modules/authentication/stores/forgot_password_store.dart
@@ -2,14 +2,10 @@ import 'package:flutter_triple/flutter_triple.dart';
 import 'package:luguel/app/modules/authentication/interfaces/i_forgot_password_store.dart';
 
 class ForgotPasswordStore extends Store<int> implements IForgotPasswordStore{
+  static const int emailMethod = 0;
+  static const int smsMethod = 1;
 
   ForgotPasswordStore() : super(0);
-
-  @override
-  int get emailMethod => 0;
-
-  @override
-  int get smsMethod => 1;
 
   @override
   void selectContactMethod(int contactMethod) {
