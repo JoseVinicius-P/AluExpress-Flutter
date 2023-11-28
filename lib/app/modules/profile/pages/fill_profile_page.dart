b89f6231e361f198e6c076f454b33653fd9900de
JoseVinicius-P <josevpf123@gmail.com>
2023-11-27 21:41:04 -0300

    style: melhorando selecionador de imagem de perfil

--- lib/app/modules/profile/pages/fill_profile_page.dart
+++ lib/app/modules/profile/pages/fill_profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:luguel/app/shared/utilities/my_colors.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class FillProfilePage extends StatefulWidget {
   const FillProfilePage({Key? key}) : super(key: key);
@@ -15,6 +16,10 @@ class FillProfilePageState extends State<FillProfilePage> {
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         forceMaterialTransparency: true,
+        leading: IconButton(
+          onPressed: (){},
+          icon: const Icon(Icons.close_rounded, color: MyColors.textColor,),
+        ),
         title: Text(
           "Complete seu perfil",
           style: theme.textTheme.titleMedium!.copyWith(color: MyColors.textColor, fontSize: 25),
@@ -22,22 +27,42 @@ class FillProfilePageState extends State<FillProfilePage> {
       ),
       body: Column(
         children: [
-          Container(
-            child: ClipOval(
-              child: Stack(
-                children: [
-                  const Image(
-                    image: AssetImage('assets/images/profile.png'),
-                    fit: BoxFit.cover,
-                  ),
-                  const Image(
-                    image: AssetImage('assets/images/profile.png'),
-                    fit: BoxFit.cover,
-                  ),
-                ],
+          const SizedBox(height: 20,),
+          SafeArea(
+            child: Center(
+              child: Container(
+                width: 40.sw,
+                height: 40.sw,
+                child: Stack(
+                  children: [
+                    const ClipOval(
+                      child: Image(
+                        image: AssetImage('assets/images/profile.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 5,
+                      right: 0,
+                      child: InkWell(
+                        onTap: (){},
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: MyColors.primaryColor,
+                            borderRadius: BorderRadius.circular(12)
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Icon(Icons.edit_rounded, color: Colors.white,),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          )
+          ),
         ],
       ),
     );
