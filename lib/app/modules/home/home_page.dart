6ed0e10f2b1c3ef473868218ce198c883eb8b1f9
JoseVinicius-P <josevpf123@gmail.com>
2023-12-03 18:27:10 -0300

    style: adicionando textfield como caixa de pesquisa a tela

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,10 +1,13 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:luguel/app/modules/home/home_store.dart';
 import 'package:flutter/material.dart';
+import 'package:luguel/app/shared/utilities/my_colors.dart';
+import 'package:luguel/app/shared/utilities/my_edge_insets.dart';
+import 'package:luguel/app/shared/widgets/text_field_widget.dart';
 
 class HomePage extends StatefulWidget {
-  final String title;
-  const HomePage({Key? key, this.title = 'HomePage'}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
   @override
   HomePageState createState() => HomePageState();
 }
@@ -13,12 +16,52 @@ class HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        forceMaterialTransparency: true,
+        title: Row(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: MyColors.primaryColor,
+                borderRadius: BorderRadius.circular(11)
+              ),
+              padding: const EdgeInsets.all(3),
+              child: const Icon(Icons.home_rounded, color: Colors.white, size: 25,)
+            ),
+            const SizedBox(width: 8,),
+            Text(
+              "Luguel",
+              style: theme.textTheme.titleMedium!.copyWith(color: MyColors.textColor, fontSize: 22),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: const Icon(Icons.notifications_active_outlined, color: MyColors.textColor)
+          )
+        ],
       ),
-      body: Column(
-        children: <Widget>[],
+      body: Padding(
+        padding: MyEdgeInsets.standard,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AutoSizeText(
+              "Olá, José 👋",
+              style: theme.textTheme.titleMedium!.copyWith(color: MyColors.textColor, fontSize: 35),
+            ),
+            SizedBox(height: 15,),
+            TextFieldWidget(
+              hint: "Pesquisar",
+              prefixIcon: Icons.search_rounded,
+              suffixIcon: Icons.tune_rounded,
+              onPressedSuffixIcon: (){},
+            ),
+          ],
+        ),
       ),
     );
   }
