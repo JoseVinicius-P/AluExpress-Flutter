2941ba378b938cbcf514b1c9799fa6ccd64415ad
JoseVinicius-P <josevpf123@gmail.com>
2023-12-07 22:25:44 -0300

    style: Adicionando lista de casas recem adicionadas

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -50,72 +50,163 @@ class HomePageState extends State<HomePage> {
         //Controller
         controller: pageViewController,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: MyEdgeInsets.standard,
-                child: Column(
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: MyEdgeInsets.standard,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AutoSizeText(
+                        "Olá, José 👋",
+                        style: theme.textTheme.titleMedium!.copyWith(color: MyColors.textColor, fontSize: 35),
+                      ),
+                      const SizedBox(height: 15,),
+                      TextFieldWidget(
+                        hint: "Pesquisar",
+                        prefixIcon: Icons.search_rounded,
+                        suffixIcon: Icons.tune_rounded,
+                        onPressedSuffixIcon: (){},
+                      ),
+                    ],
+                  ),
+                ),
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    AutoSizeText(
-                      "Olá, José 👋",
-                      style: theme.textTheme.titleMedium!.copyWith(color: MyColors.textColor, fontSize: 35),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(width: MyEdgeInsets.standard.left,),
+                          SimpleFilterWidget(textFilter: "Bem avaliadas", onTap: (){}, isSelected: true,),
+                          const SizedBox(width: 5,),
+                          SimpleFilterWidget(textFilter: "Mais baratas", onTap: (){}, isSelected: false,),
+                          const SizedBox(width: 5,),
+                          SimpleFilterWidget(textFilter: "Mais Caras", onTap: (){}, isSelected: false,),
+                          const SizedBox(width: 5,),
+                          SimpleFilterWidget(textFilter: "Em alta", onTap: (){}, isSelected: false,),
+                          SizedBox(width: MyEdgeInsets.standard.right,),
+                        ],
+                      ),
                     ),
-                    const SizedBox(height: 15,),
-                    TextFieldWidget(
-                      hint: "Pesquisar",
-                      prefixIcon: Icons.search_rounded,
-                      suffixIcon: Icons.tune_rounded,
-                      onPressedSuffixIcon: (){},
+                    const SizedBox(height: 20,),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(width: MyEdgeInsets.standard.left,),
+                          const ItemHouseWidget(),
+                          const SizedBox(width: 15,),
+                          const ItemHouseWidget(),
+                          const SizedBox(width: 15,),
+                          const ItemHouseWidget(),
+                          const SizedBox(width: 15,),
+                          const ItemHouseWidget(),
+                          const SizedBox(width: 15,),
+                          SizedBox(width: MyEdgeInsets.standard.right,),
+                        ],
+                      ),
                     ),
                   ],
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SizedBox(width: MyEdgeInsets.standard.left,),
-                        SimpleFilterWidget(textFilter: "Bem avaliadas", onTap: (){}, isSelected: true,),
-                        const SizedBox(width: 5,),
-                        SimpleFilterWidget(textFilter: "Mais baratas", onTap: (){}, isSelected: false,),
-                        const SizedBox(width: 5,),
-                        SimpleFilterWidget(textFilter: "Mais Caras", onTap: (){}, isSelected: false,),
-                        const SizedBox(width: 5,),
-                        SimpleFilterWidget(textFilter: "Em alta", onTap: (){}, isSelected: false,),
-                        SizedBox(width: MyEdgeInsets.standard.right,),
-                      ],
-                    ),
+                Padding(
+                  padding: MyEdgeInsets.standard,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Add recentemente",
+                            style: theme.textTheme.titleMedium!.copyWith(color: MyColors.textColor, fontSize: 18),
+                          ),
+                          Text(
+                            "Ver tudo",
+                            style: theme.textTheme.titleMedium!.copyWith(color: MyColors.primaryColor, fontSize: 16, fontWeight: FontWeight.normal),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 15,),
+                      ListView.separated(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.05),
+                                  blurRadius: 5.0,
+                                  spreadRadius: 1,
+                                ),
+                              ]
+                            ),
+                            padding: MyEdgeInsets.standard,
+                            child: Row(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(15),
+                                  child: const Image(
+                                    image: AssetImage('assets/images/house.png'),
+                                    width: 100,
+                                    height: 100,
+                                    fit: BoxFit.fitWidth,
+                                  ),
+                                ),
+                                const SizedBox(width: 10,),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Casa grande",
+                                      style: theme.textTheme.titleMedium!.copyWith(color: MyColors.textColor, fontSize: 20),
+                                    ),
+                                    Text(
+                                      "Iaciara - GO",
+                                      style: theme.textTheme.titleMedium!.copyWith(fontSize: 15, fontWeight: FontWeight.w300, color: Colors.grey),
+                                      textAlign: TextAlign.left,
+                                    ),
+                                    Row(
+                                      children: [
+                                        const Icon(Icons.star_rounded, color: Colors.yellow, size: 15,),
+                                        const SizedBox(width: 3,),
+                                        AutoSizeText(
+                                          "4,8",
+                                          style: theme.textTheme.labelSmall!.copyWith(color: MyColors.grey),
+                                        ),
+                                        const SizedBox(width: 3,),
+                                        Text(
+                                          "(150 avaliações)",
+                                          style: theme.textTheme.titleMedium!.copyWith(fontSize: 13, fontWeight: FontWeight.w300, color: Colors.grey),
+                                          textAlign: TextAlign.left,
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                        separatorBuilder: (context, index) => const SizedBox(height: 15,),
+                        itemCount: 25)
+                    ],
                   ),
-                  const SizedBox(height: 20,),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SizedBox(width: MyEdgeInsets.standard.left,),
-                        const ItemHouseWidget(),
-                        const SizedBox(width: 15,),
-                        const ItemHouseWidget(),
-                        const SizedBox(width: 15,),
-                        const ItemHouseWidget(),
-                        const SizedBox(width: 15,),
-                        const ItemHouseWidget(),
-                        const SizedBox(width: 15,),
-                        SizedBox(width: MyEdgeInsets.standard.right,),
-                      ],
-                    ),
-                  ),
-                ],
-              )
-            ],
+                )
+              ],
+            ),
           ),
           SizedBox(),
           SizedBox(),
