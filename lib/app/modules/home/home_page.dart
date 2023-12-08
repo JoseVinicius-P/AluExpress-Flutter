2648fd08a1db623dda5a1f271241433d58e61660
JoseVinicius-P <josevpf123@gmail.com>
2023-12-07 21:24:32 -0300

    refactor: Organizando código

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -124,61 +124,75 @@ class HomePageState extends State<HomePage> {
       ),
 
       //Criando o BottomNavigation
-      bottomNavigationBar: AnimatedBuilder(
-        //Possibilitando a troca da current page usando o controller do PageView
-          animation: pageViewController,
-          builder: (context, snapshot) {
-            return Container(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.vertical(top: Radius.circular(16.0)),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.05),
-                    blurRadius: 5.0,
-                    spreadRadius: 1,
+      bottomNavigationBar: BottomNavigationBarWidget(pageViewController: pageViewController),
+    );
+  }
+}
+
+class BottomNavigationBarWidget extends StatelessWidget {
+  const BottomNavigationBarWidget({
+    super.key,
+    required this.pageViewController,
+  });
+
+  final PageController pageViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      //Possibilitando a troca da current page usando o controller do PageView
+        animation: pageViewController,
+        builder: (context, snapshot) {
+          return Container(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.vertical(top: Radius.circular(16.0)),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.05),
+                  blurRadius: 5.0,
+                  spreadRadius: 1,
+                ),
+              ],
+            ),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.vertical(top: Radius.circular(20.0)),
+              child: BottomNavigationBar(
+                showSelectedLabels: true,
+                showUnselectedLabels: true,
+                backgroundColor: Colors.transparent,
+                currentIndex: pageViewController.page?.round() ?? 0,
+                selectedItemColor: MyColors.primaryColor,
+                unselectedItemColor: Colors.grey,
+                onTap: (index){
+                  pageViewController.animateToPage(index, // Índice da página para animar
+                    duration: const Duration(milliseconds: 800), // Duração da animação
+                    curve: Curves.easeInOutCubicEmphasized, // Curva de animação
+                  );
+                },
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home_rounded),
+                    label: 'Início',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.search_rounded),
+                    label: 'Pesquisar'
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.add_home),
+                    label: 'Casas'
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.person_rounded),
+                    label: 'Perfil'
                   ),
                 ],
               ),
-              child: ClipRRect(
-                borderRadius: const BorderRadius.vertical(top: Radius.circular(20.0)),
-                child: BottomNavigationBar(
-                  showSelectedLabels: true,
-                  showUnselectedLabels: true,
-                  backgroundColor: Colors.transparent,
-                  currentIndex: pageViewController.page?.round() ?? 0,
-                  selectedItemColor: MyColors.primaryColor,
-                  unselectedItemColor: Colors.grey,
-                  onTap: (index){
-                    pageViewController.animateToPage(index, // Índice da página para animar
-                      duration: const Duration(milliseconds: 800), // Duração da animação
-                      curve: Curves.easeInOutCubicEmphasized, // Curva de animação
-                    );
-                  },
-                  items: const <BottomNavigationBarItem>[
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.home_rounded),
-                      label: 'Início',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.search_rounded),
-                      label: 'Pesquisar'
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.add_home),
-                      label: 'Casas'
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.person_rounded),
-                      label: 'Perfil'
-                    ),
-                  ],
-                ),
-              ),
-            );
-          }
-      ),
+            ),
+          );
+        }
     );
   }
 }
@@ -215,11 +229,11 @@ class ItemHouseWidget extends StatelessWidget {
                           borderRadius: BorderRadius.circular(20),
                           color: MyColors.primaryColor
                         ),
-                        padding: EdgeInsets.symmetric(vertical: 3, horizontal: 10),
+                        padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 10),
                         child: Row(
                           children: [
-                            Icon(Icons.star_rounded, color: MyColors.iconButtonColor, size: 15,),
-                            SizedBox(width: 3,),
+                            const Icon(Icons.star_rounded, color: MyColors.iconButtonColor, size: 15,),
+                            const SizedBox(width: 3,),
                             AutoSizeText(
                               "4,8",
                               style: theme.textTheme.labelSmall,
