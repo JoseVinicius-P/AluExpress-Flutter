6fe5a8c2eb61ef4ffe141a3761a105bc8ca0a7a0
JoseVinicius-P <josevpf123@gmail.com>
2023-12-09 19:05:10 -0300

    style: Diminuindo espessura do divider_widget

--- lib/app/shared/widgets/divider_widget.dart
+++ lib/app/shared/widgets/divider_widget.dart
@@ -17,14 +17,14 @@ class DividerWidget extends StatelessWidget {
       child: Row(
         children: [
           SizedBox(width: verticalPadding),
-          Expanded(child: Container(color: MyColors.grey.withOpacity(0.5), height: 1)),
+          Expanded(child: Container(color: MyColors.grey.withOpacity(0.5), height: 0.5)),
           if(text != null) const SizedBox(width: 8,),
           if(text != null)  Text(
             text ?? '',
             style: theme.textTheme.labelSmall!.copyWith(color: MyColors.textColor.withOpacity(0.6), fontWeight: FontWeight.normal, fontSize: 18),
           ),
           if(text != null)  const SizedBox(width: 8,),
-          Expanded(child: Container(color: MyColors.grey.withOpacity(0.5), height: 1)),
+          Expanded(child: Container(color: MyColors.grey.withOpacity(0.5), height: 0.5)),
           SizedBox(width: verticalPadding),
         ],
       ),
