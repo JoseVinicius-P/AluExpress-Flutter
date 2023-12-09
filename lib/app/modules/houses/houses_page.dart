8dbddc3252fa57cd21339e8e94337a5019ab1221
JoseVinicius-P <josevpf123@gmail.com>
2023-12-09 18:33:38 -0300

    style: Adicionando CarouselSliderWidget

--- lib/app/modules/houses/houses_page.dart
+++ lib/app/modules/houses/houses_page.dart
@@ -1,6 +1,11 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:luguel/app/modules/houses/houses_store.dart';
 import 'package:flutter/material.dart';
+import 'package:luguel/app/shared/utilities/my_colors.dart';
+import 'package:luguel/app/shared/widgets/carousel_slider_widget.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class HousesPage extends StatefulWidget {
   const HousesPage({Key? key}) : super(key: key);
@@ -9,15 +14,48 @@ class HousesPage extends StatefulWidget {
 }
 class HousesPageState extends State<HousesPage> {
   final HousesStore store = Modular.get();
+  final CarouselController _carouselController = CarouselController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: Text(""),
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        iconTheme: const IconThemeData(
+            color: MyColors.iconButtonColor,
+            shadows: [
+              BoxShadow(
+                color: Colors.black,
+                offset: Offset(0, 0),
+                blurRadius: 20,
+              )
+            ]
+        ),
+        forceMaterialTransparency: true,
       ),
       body: Column(
-        children: <Widget>[],
+        children: [
+          CarouselSliderWidget(
+            carouselController: _carouselController,
+            onPageChanged: (index) {},
+            axis: Axis.horizontal,
+            height: 100.sw * 0.8,
+            autoPlay: true,
+            autoPlayInterval: const Duration(seconds: 10),
+            enableInfiniteScroll: true,
+            children: [
+              Container(
+                constraints: const BoxConstraints.expand(),
+                child: Image(
+                  image: AssetImage('assets/images/house.png'),
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ],
+          ),
+
+        ],
       ),
     );
   }
